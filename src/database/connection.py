f791fccfa659c29b659c5d62263095104d146100
unknown <wesleyl.oliveira@outlook.com>
2026-08-09 11:19:06 -0300

    Correção da chamada da função 'connect'

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,8 +1,8 @@
 import os
-from psycopg import Connection
+from psycopg import Connection, connect
 
 def get_connection() -> Connection:
-    return psycopg.connect(
+    return connect(
         host=os.getenv("POSTGRES_HOST"),
         port=os.getenv("POSTGRES_PORT"),
         dbname=os.getenv("POSTGRES_DB"),
