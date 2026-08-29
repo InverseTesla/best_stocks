a8e7fdac7ca91caed19f844f05175a4d57cfa5f9
unknown <wesleyl.oliveira@outlook.com>
2026-08-29 15:03:08 -0300

    Ajustando a quantidade de placeholders aos parâmetros e incluindo o logger.

--- src/database/repository.py
+++ src/database/repository.py
@@ -1,38 +1,42 @@
+from src.utils.logger import logger
 from src.database.connection import get_connection
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
 def insert_stock_metric(df):
 
-    df.columns = df.columns.str.strip()
-    
-    with get_connection() as conn:
-        with conn.cursor() as cur:
+    try:
+        df.columns = df.columns.str.strip()
 
-            cur.execute("""
-                INSERT INTO stock_metrics (
-                    ticker,
-                    price,
-                    dividend_yield,
-                    price_to_earnings,
-                    price_to_book,
-                    roe,
-                    daily_liquidity,
-                    debt_to_equity,
-                    annual_recurring_revenue,
-                    collected_at
-                )
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
-            """, (
-                df['TICKER'],
-                df['PRECO'],
-                df['DY'],
-                df['P/L'],
-                df['P/VP'],
-                df['ROE'],
-                df['LIQUIDEZ MEDIA DIARIA'],
-                df['DIV. LIQ. / PATRI.'],
-                df['CAGR RECEITAS 5 ANOS'],
-                datetime.now(ZoneInfo("America/Sao_Paulo"))
-            ))
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+
+                cur.execute("""
+                    INSERT INTO stock_metrics (
+                        ticker,
+                        price,
+                        dividend_yield,
+                        price_to_earnings,
+                        price_to_book,
+                        roe,
+                        daily_liquidity,
+                        debt_to_equity,
+                        annual_recurring_revenue,
+                        collected_at
+                    )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                """, (
+                    df['TICKER'],
+                    df['PRECO'],
+                    df['DY'],
+                    df['P/L'],
+                    df['P/VP'],
+                    df['ROE'],
+                    df['LIQUIDEZ MEDIA DIARIA'],
+                    df['DIV. LIQ. / PATRI.'],
+                    df['CAGR RECEITAS 5 ANOS'],
+                    datetime.now(ZoneInfo("America/Sao_Paulo"))
+                ))
+    except Exception as e:
+        logger.error("Erro ao inserir das no banco: %s", e)
 
